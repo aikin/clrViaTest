f1c989477f76d914b546a26f3f74bdc491fe084d
aikin <me@ulaijn.com>
2016-05-28 15:38:17 +0800

    aikin; enums

--- src/BanKai.Basic/10_NestedTypes.cs
+++ src/BanKai.Basic/10_NestedTypes.cs
@@ -9,12 +9,12 @@ namespace BanKai.Basic
         [Fact]
         public void should_connect_with_plus_sign_when_get_type_of_nested_class()
         {
-            Type typeOfNestedType = typeof(NestedTypeDemoClass.NestedType);
+            var typeOfNestedType = typeof(NestedTypeDemoClass.NestedType);
             
-            string fullName = typeOfNestedType.FullName;
+            var fullName = typeOfNestedType.FullName;
 
             // correct the value of expectedFullName to fix the test.
-            const string expectedFullName = "BanKai.Basic.Common.NestedTypeDemoClass.NestedType";
+            const string expectedFullName = "BanKai.Basic.Common.NestedTypeDemoClass+NestedType";
 
             Assert.Equal(expectedFullName, fullName);
         }
